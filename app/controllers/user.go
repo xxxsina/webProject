a8dc5f2e7abd249b287031154286d39f04d5b944
lihao <1340691@qq.com>
2019-12-16 09:18:49 +0800

    修改

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"log"
+	"github.com/go-playground/validator"
 	"net/http"
 	"webProject/com_party/helper"
 	"webProject/com_party/libraries/Cache"
@@ -13,9 +13,25 @@ import (
 
 //这里提供一个参数提交的绑定数据结构
 type UserInfo struct {
-	Id int `form:"id" json:"id"`
-	Name string `form:"name" json:"name"`
-	Mobile string `form:"name" json:"mobile"`
+	Id     int    `form:"id" json:"id" binding:"required"`
+	Name   string `form:"name" json:"name" binding:"required"`
+	Mobile string `form:"mobile" json:"mobile" binding:"required"`
+}
+
+// 绑定模型获取验证错误的方法
+func (r *UserInfo) GetError(errs validator.ValidationErrors) (int, string) {
+	fmt.Println("******************")
+	fmt.Println(errs)
+	fmt.Println("******************")
+	for _, e := range errs {
+		if e.Field() == "Mobile" {
+			switch e.Tag() {
+				case "required":
+					return 10000, "请输入手机号码"
+			}
+		}
+	}
+	return 0, "参数错误"
 }
 
 func GetName(c *gin.Context) {
@@ -45,7 +61,12 @@ func GetName(c *gin.Context) {
 	//声明、绑定 http://127.0.0.1:8080/name/7?id=2
 	var uinfo UserInfo
 	if err := c.ShouldBind(&uinfo); err != nil {
-		log.Fatal("bind user info failure :", err)
+		fmt.Println(err.(validator.ValidationErrors))
+		//code, msg := uinfo.GetError(err.(validator.ValidationErrors))
+		//fmt.Println(code)
+		//fmt.Println(msg)
+		//log.Fatal("bind user info failure :", code, msg)
+		return
 	}
 	//绑定的获取参数方式
 	r := service.User{ID: uinfo.Id}
@@ -59,20 +80,20 @@ func GetName(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{
-			"code" : helper.Code0,
-			"massage" : err,
+			"code":    helper.Code0,
+			"massage": err,
 		})
 	} else {
 		c.JSON(http.StatusOK, gin.H{
-			"code" : helper.Code200,
-			"massage" : helper.CodeText(helper.Code200),
-			"token" : token,
-			"data" : u,
+			"code":    helper.Code200,
+			"massage": helper.CodeText(helper.Code200),
+			"token":   token,
+			"data":    u,
 		})
 	}
 }
 
-func Add(c *gin.Context)  {
+func Add(c *gin.Context) {
 	var name, mobile string
 	fmt.Println("==================")
 	//var v middleware.CustomClaims
@@ -86,16 +107,16 @@ func Add(c *gin.Context)  {
 	var uinfo UserInfo
 	if err := c.ShouldBindJSON(&uinfo); err != nil {
 		c.JSON(http.StatusOK, gin.H{
-			"code" : helper.Code10003,
-			"message" : helper.CodeText(helper.Code10003),
-			"data" : nil,
+			"code":    helper.Code10003,
+			"message": helper.CodeText(helper.Code10003),
+			"data":    nil,
 		})
 		return
 	} else {
 		name = uinfo.Name
 		mobile = uinfo.Mobile
 	}
-	r := service.User{Name:name, Mobile: mobile}
+	r := service.User{Name: name, Mobile: mobile}
 	u, err := r.InsertUser()
 	if err != nil {
 		c.String(http.StatusInternalServerError, err.Error())
@@ -103,8 +124,8 @@ func Add(c *gin.Context)  {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"code" : helper.Code200,
-		"massage" : helper.CodeText(helper.Code200),
-		"data" : u,
+		"code":    helper.Code200,
+		"massage": helper.CodeText(helper.Code200),
+		"data":    u,
 	})
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,14 @@ go 1.13
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.4.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.30.2+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-xorm/xorm v0.7.9
+	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.2.0
+	xorm.io/builder v0.3.6
 	xorm.io/core v0.7.2-0.20190928055935-90aeac8d08eb
 )
